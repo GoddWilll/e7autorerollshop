2592c271c4554bc9a2e9b6acb2871399d521ee4e
GoddWilll <202543@umons.ac.be>
2022-01-30 01:38:12 +0100

    Update v3

--- main.py
+++ main.py
@@ -33,9 +33,11 @@ def main():
     
     
 def initialize():
-    print("Starting the count", end="")
+    print("AUTOMATIC REROLL SHOP V0.1 by Godwill#9999\n\n")
+    print("----------------------------------------------\n")
+    print("Starting the script ", end="")
     for i in range(0, 3):
-        print(" .", end="")
+        print(". ", end="")
         time.sleep(1)
     print("Go")
     
@@ -62,10 +64,10 @@ def exists(element):
     )
     researched_elem = pyautogui.locateOnScreen(item_path, confidence=0.99)
     if researched_elem == None:
-        print(element + " not found!")
+        #print(element + " not found!")
         return False
     else : 
-        print("I found " + element)
+        #print("I found " + element)
         return researched_elem
 
 
@@ -79,6 +81,19 @@ def bought():
         
 def buy_item():
     location = exists('buy_cov.png')
+    location2 = exists('buy_myst.png')
+    
+    if location2 != False:
+        script_dir = os.path.dirname(__file__)
+        item_path = os.path.join(
+            script_dir,
+            'imgs',
+            'buy_myst.png'
+        )
+        x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
+        pyautogui.click(x, y)
+        price_check(0)
+        
     if location !=False:
         script_dir = os.path.dirname(__file__)
         item_path = os.path.join(
@@ -87,7 +102,6 @@ def buy_item():
             'buy_cov.png'
         )
         x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
-        time.sleep(0.3)
         pyautogui.click(x, y)
         price_check(0)
         
@@ -95,8 +109,8 @@ def buy_item():
 def price_check(num):
     time.sleep(0.5)
     location = exists('5bm.png')
-    location2 = exists('myst_price.png')
-    
+    location2 = exists('50bm.png')
+    print(location2)
     if location == False and location2 == False:
         if num == 0:
             move_page()
@@ -120,17 +134,16 @@ def price_check(num):
                 
         if location2 != False:
             script_dir = os.path.dirname(__file__)
-            if location != False:  
-                item_path = os.path.join(
-                    script_dir,
-                    'imgs',
-                    'myst_price.png'
-                )
-                x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
-                pyautogui.click(x, y+50)
+            item_path = os.path.join(
+                script_dir,
+                'imgs',
+                '50bm.png'
+            )
+            x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
+            pyautogui.click(x + 500, y+40)
+            buy_item()
+            if bought() == False:
                 buy_item()
-                if bought() == False:
-                    buy_item()
     
         
        
@@ -159,13 +172,13 @@ def refresh_click(rc):
         )
         x, y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
         pyautogui.click(x, y)
-        time.sleep(0.5)
+        time.sleep(1)
         return rc+1
     
     
 def move_page():
     pyautogui.scroll(-10)
-    time.sleep(0.5)
+    time.sleep(1)
 
         
 if __name__ == '__main__':
