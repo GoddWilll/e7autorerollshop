743064fd015991b08f04cdfe086e6d461ce130a0
GoddWilll <202543@umons.ac.be>
2022-01-30 01:05:05 +0100

    Update v;2

---
+++ imgs/try.py
@@ -0,0 +1,4 @@
+if 2==3 or 4==5:
+    print("hello")
+else :
+    print("fff")
--- main.py
+++ main.py
@@ -2,40 +2,29 @@ import pyautogui
 import time
 import os
 
-"""class Script():
-    def __init__(self):
-        self.name = "Script"
-        self.counter = 0
-        
-    def update(self):
-        self.counter += 1"""
-    
 
 def main():
     
-     # initialized refresh counter variable
+    # initialized refresh counter variable
     refreshCounter = 0
     
+    # calibrating the autoclicker
+    calibration()
+    
+    # loop
     while(True):
         
         # initialized pyautogui
         pyautogui.FAILSAFE = True
+        
+        pyautogui.screenshot("VERIFY.png")
 
         # geting the position for the bm
-        get_bm_pos()
-        
-        # waiting 
-        time.sleep(1)
+        price_check(0)
         
         # getting the position + refreshing
-        refresh_click()
-        
-        # udpdating the refresh counter
-        refreshCounter +=1
-        
-        # waiting
-        time.sleep(1)
-        
+        refreshCounter = refresh_click(refreshCounter)        
+
         # getting the position + refreshing    
         confirm_click()
         
@@ -46,12 +35,24 @@ def main():
 def initialize():
     print("Starting the count", end="")
     for i in range(0, 3):
-        print(".", end="")
+        print(" .", end="")
         time.sleep(1)
     print("Go")
     
-    """Function made to test if the element exists on the page
-    """
+    
+def calibration():
+    location = exists('pos_calibration.png')
+    if location != False:
+        script_dir = os.path.dirname(__file__)
+        item_path = os.path.join(
+            script_dir,
+            'imgs',
+            'pos_calibration.png'
+        )
+        x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
+        pyautogui.click(x,y)
+ 
+ 
 def exists(element):
     script_dir = os.path.dirname(__file__)
     item_path = os.path.join(
@@ -59,24 +60,81 @@ def exists(element):
         'imgs',
         element
     )
-    researched_elem = pyautogui.locateOnScreen(item_path, confidence=0.9)
+    researched_elem = pyautogui.locateOnScreen(item_path, confidence=0.99)
     if researched_elem == None:
+        print(element + " not found!")
         return False
     else : 
+        print("I found " + element)
         return researched_elem
 
-def price_check():
-    location = exists('price.png')
+
+def bought():
+    location = exists('bought.png')
+    if location !=False:
+        return True
+    else : 
+        return False
+        
+        
+def buy_item():
+    location = exists('buy_cov.png')
     if location !=False:
         script_dir = os.path.dirname(__file__)
         item_path = os.path.join(
             script_dir,
             'imgs',
-            'price.png'
+            'buy_cov.png'
         )
         x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
-        pyautogui.click(x, y+50)
+        time.sleep(0.3)
+        pyautogui.click(x, y)
+        price_check(0)
+        
+
+def price_check(num):
+    time.sleep(0.5)
+    location = exists('5bm.png')
+    location2 = exists('myst_price.png')
     
+    if location == False and location2 == False:
+        if num == 0:
+            move_page()
+            time.sleep(0.5)
+            price_check(1)
+        else :
+            return
+    else:
+        script_dir = os.path.dirname(__file__)
+        if location != False:  
+            item_path = os.path.join(
+                script_dir,
+                'imgs',
+                '5bm.png'
+            )
+            x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
+            pyautogui.click(x + 500, y+40)
+            buy_item()
+            if bought() == False:
+                buy_item()
+                
+        if location2 != False:
+            script_dir = os.path.dirname(__file__)
+            if location != False:  
+                item_path = os.path.join(
+                    script_dir,
+                    'imgs',
+                    'myst_price.png'
+                )
+                x,y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
+                pyautogui.click(x, y+50)
+                buy_item()
+                if bought() == False:
+                    buy_item()
+    
+        
+       
+                
 def confirm_click():
     location = exists('confirm_button.png')
     if location != False:
@@ -90,30 +148,7 @@ def confirm_click():
         pyautogui.click(x,y)
         
 
-def get_bm_pos():
-    location = exists('part_bm.png')
-    if location != False:
-        time.sleep(0.5)
-        price_check()
-    else :
-        time.sleep(1)
-        move_page()
-        if exists('part_bm.png') != False:
-            price_check()
-            
-            """"script_dir = os.path.dirname(__file__)
-            item_path = os.path.join(
-            script_dir,
-            'imgs',
-            'part_bm.png'
-            )
-            x, y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
-            pyautogui.moveTo(x,y)
-            time.sleep(0.5)
-            pyautogui.click(x+300,y)"""
-        
-
-def refresh_click():
+def refresh_click(rc):
     location = exists('refresh.png')
     if location != False:
         script_dir = os.path.dirname(__file__)
@@ -124,25 +159,14 @@ def refresh_click():
         )
         x, y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
         pyautogui.click(x, y)
+        time.sleep(0.5)
+        return rc+1
     
     
-def py_screenshot():
-    im = pyautogui.screenshot('Screenshot_test.png')
-
 def move_page():
-    location = exists('pos_calibration.png')
-    if location != False:
-        script_dir = os.path.dirname(__file__)
-        item_path = os.path.join(
-            script_dir,
-            'imgs',
-            'pos_calibration.png'
-        )    
-        x, y = pyautogui.locateCenterOnScreen(item_path, confidence=0.9)
-        pyautogui.moveTo(x,y+100)
-        pyautogui.dragTo(x, y-150,1, pyautogui.easeInQuad, button='left')
+    pyautogui.scroll(-10)
+    time.sleep(0.5)
 
-        
         
 if __name__ == '__main__':
     
